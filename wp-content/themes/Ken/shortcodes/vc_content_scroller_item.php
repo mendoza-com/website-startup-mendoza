d80e85ff175f9f6a5af5259121d78a3c0f44c6fe
mtorresvc <marcos@viacelular.com>
2017-03-08 15:50:02 -0300

    Fix plugins

--- wp-content/themes/Ken/shortcodes/vc_content_scroller_item.php
+++
@@ -1,8 +0,0 @@
-<?php
-$output = '';
-$output .= '<div class="swiper-slide">';
-$output .= '<div class="mk-inner-grid">';
-$output .= wpb_js_remove_wpautop($content);
-$output .= '</div>';
-$output .= '</div>';
-echo $output;
